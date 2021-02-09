c205c107efd3ae1c8f6021b82b5ae09cd325fa45
Jane <73879019+Jane-UA@users.noreply.github.com>
2021-02-09 14:37:18 +0200

    tasks

---
+++ task1.py
@@ -0,0 +1,6 @@
+# The greeting program
+from datetime import datetime
+current_date = datetime.now().date()
+
+name = 'Snezhana!'
+print('Good day, ', name, current_date,'is a perfect day to learn python.')
